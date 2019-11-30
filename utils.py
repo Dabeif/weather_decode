a60d1c5ac1213ab3ccb76273b9818b5ea3f2bbca
Kevin234-li <kevin234.li@qq.com>
2019-11-30 20:44:46 +0800

    update

--- utils.py
+++ utils.py
@@ -33,30 +33,30 @@ def IIiii(item: str):
     if ans.empty:
         ans = "未知"
     else:
-        ans = ans.iloc[0][1] + ans.iloc[0][2]
+        ans = f"{'城市':　<6}: " + ans.iloc[0][1] + ans.iloc[0][2]
     return ans
 
 
 def ir_ix_h_vv(item: str):
     ans = ""
     if int(item[1]) <= 3:
-        ans += '人工站：'
+        ans += f"{'人工站':　<6}: "
     else:
-        ans += '自动站'
+        ans += f"{'自动站':　<6}: "
     # ir:是否编报降水组
     if item[0] != '/':
         ans += ('不' if item[0] != '1' else '') + '编报降水组, '
     # 是否编报天气现象组
     if item[1] != '/':
-        ans += ('不' if item[1] != '1' else '') + '编报天气现象组, '
+        ans += ('不' if item[1] != '1' else '') + '编报天气现象组\n'
     # h: 云底高度
-    ans += '云底高度：'
+    ans += f"{'云底高度':　<6}: "
     if item[2] != '/':
-        ans += f'{data.cloud_bottom_height[item[2]]}, '
+        ans += f'{data.cloud_bottom_height[item[2]]}\n'
     else:
         ans += '缺测'
     # vv: 有效能见度
-    ans += '有效能见度：'
+    ans += f"{'有效能见度':　<6}: "
     if item[3:] != '//':
         ans += f'{effective_visibility(int(item[3:]))}千米'
     else:
@@ -67,25 +67,28 @@ def ir_ix_h_vv(item: str):
 def N_dd_ff(item: str):
     ans = ''
     # N: 总云量
+    ans += f"{'总云量':　<6}: "
     if item[0] != '/':
-        ans += f'总云量为{data.cloud_amount[item[0]]}, '
+        ans += f'{data.cloud_amount[item[0]]}\n'
     else:
-        ans += '总云量缺测'
+        ans += '缺测\n'
     # dd: 风向
+    ans += f"{'风向':　<6}: "
     if item[1:3] != '//':
-        ans += f'{data.wind_direction[item[1:3]]}风, '
+        ans += f'{data.wind_direction[item[1:3]]}风\n'
     else:
-        ans += '风向缺测，'
+        ans += '缺测\n'
     # ff: 风速
+    ans += f"{'风速':　<6}: "
     if item[3:] != '//':
-        ans += f'风速{int(item[3:])}米/秒'
+        ans += f'{int(item[3:])}米/秒'
     else:
-        ans += '风速缺测'
+        ans += '缺测'
     return ans
 
 
 def _1Sn_TTT(item: str):
-    ans = '温度：'
+    ans = f"{'温度':　<6}: "
     if item[1] == '1':
         ans += '-'
     else:
@@ -98,7 +101,7 @@ def _1Sn_TTT(item: str):
 
 
 def _2Sn_TdTdTd(item: str):
-    ans = '露点：'
+    ans = f"{'露点':　<6}: "
     if item[1] == '1':
         ans += '-'
     else:
@@ -111,7 +114,7 @@ def _2Sn_TdTdTd(item: str):
 
 
 def _3PoPoPoPo(item: str):
-    ans = '气压：'
+    ans = f"{'气压':　<6}: "
     if item[1:] != '////':
         ans += f'{int(item[1:]) / 10}hPa'
     else:
@@ -120,7 +123,7 @@ def _3PoPoPoPo(item: str):
 
 
 def _4PPPP(item: str):
-    ans = '海平面气压：'
+    ans = f"{'海平面气压':　<6}: "
     if item[1:] != '////':
         ans += f'{1000 + int(item[1:]) / 10}hPa'
     else:
@@ -129,7 +132,7 @@ def _4PPPP(item: str):
 
 
 def _5aPPP(item: str):
-    ans = f'过去三小时内气压{data.pressure_change[item[1]]}'
+    ans = f"{'过去三小时内':　<6}: " + f'气压{data.pressure_change[item[1]]}'
     if item[1] == '2':
         ans += f', 气压变量为+{int(item[2:]) / 10}hPa'
     elif item[1] == '7':
@@ -138,7 +141,7 @@ def _5aPPP(item: str):
 
 
 def _6RRR1(item: str):
-    ans = '过去六小时内本站降水量:'
+    ans = f"{'过去六小时内':　<6}: " + '本站降水量'
     t = int(item[1:-1])
     if t != '///':
         if 1 <= t <= 988:
@@ -156,12 +159,12 @@ def _7wwW1W2(item: str):
     ww = item[1:3]
     W1 = item[3]
     W2 = item[4]
-    ans = '现在天气现象:'
+    ans = f"{'现在天气现象':　<6}: "
     if ww != "//":
         ans += f'{data.weather_phenomenon[ww]}\n'
     else:
         ans += '缺测\n'
-    ans += '过去天气现象：'
+    ans += f"{'过去天气现象':　<6}: "
     if W1 != '/':
         ans += f'{data.past_weather_phenomenon[W1]}'
     if W2 != '/':
@@ -173,21 +176,21 @@ def _8Nh_CCC(item: str):
     ans = ''
     if item[1] != '/':
         if item[2] != '0':
-            ans += f'低云量为{data.cloud_amount[item[1]]}, '
+            ans += f"{'低云量':　<6}: " + f'{data.cloud_amount[item[1]]}\n'
         else:
-            ans += f'中云量为{data.cloud_amount[item[1]]}, '
+            ans += f"{'中云量':　<6}: " + f'{data.cloud_amount[item[1]]}\n'
     if item[2] != '/':
-        ans += f'低云状：{data.low_cloud[item[2]]}, '
+        ans += f"{'低云状':　<6}: " + f'{data.low_cloud[item[2]]}\n'
     else:
-        ans += f'低云状：缺测，'
+        ans += f"{'低云状':　<6}: 缺测\n"
     if item[3] != '/':
-        ans += f'中云状：{data.middle_cloud[item[3]]}, '
+        ans += f"{'中云状':　<6}: " + f'{data.middle_cloud[item[3]]}\n'
     else:
-        ans += f'中云状：缺测，'
+        ans += f"{'中云状':　<6}: 缺测\n"
     if item[4] != '/':
-        ans += f'高云状：{data.high_cloud[item[4]]}'
+        ans += f"{'高云状':　<6}:" + f'{data.high_cloud[item[4]]}'
     else:
-        ans += f'高云状：缺测'
+        ans += f"{'高云状':　<6}: 缺测"
     return ans
 
 
@@ -205,7 +208,7 @@ def translate(msg: list):
             not_recorded.remove(n)
             ans[item] = func_list[n-1](item)
     if not not_recorded:
-        ans['缺测'] = '\n缺测：无\n'
+        ans['缺测'] = '\n缺测:无\n'
     else:
         ans['缺测'] = '\n缺测:\n'
         for i in not_recorded:
@@ -243,7 +246,7 @@ def search(filepath: str, station: str):
                     msg.extend(ans.group(1).split())
                 # 翻译目标报文
                 try:
-                    print(msg)
+                    print(f"\n报文是: {msg}")
                     result = translate(msg)
                     return '\n'.join(list(result.values()))
                 except Exception:
